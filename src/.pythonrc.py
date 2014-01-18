4172fa43c9414784d90c8ae754dbae20cac1d5bf
Steve Jones <steve@secretvolcanobase.org>
2014-01-18 22:59:27 +0000

    Create history file if it doesn't exist.

--- src/.pythonrc.py
+++ src/.pythonrc.py
@@ -1,9 +1,13 @@
 import os
+import os.path
 import readline
 import atexit
 import rlcompleter
 
 history_file = os.path.expanduser('~/.python_history')
+if not os.path.exists(history_file):
+    open(history_file, "wb").close()
+
 readline.read_history_file(history_file)
 atexit.register(readline.write_history_file, history_file)
 
